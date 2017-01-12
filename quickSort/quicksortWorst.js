e8d6c63dfcfbb9cd92ec1568b45767efff69d4e1
Eric Richter <eric.richter97@gmx.de>
2017-01-12 15:18:34 +0100

    quicksort - worst case | Text/Beschreibung hinzugefügt

--- quickSort/quicksortWorst.js
+++ quickSort/quicksortWorst.js
@@ -169,54 +169,72 @@ function buttonItertator() {
 
     switch (schritt) {
         case 1:
-            setText('Darstellung des worst case von Quicksort');
+            setText('"Quicksort" ist ein Algorithmus, der verwendet wird, um eine Folge von Zahlen zu sortieren.');
             schritt++;
             break;
         case 2:
-            showKlammer(1,9);
+            setText('In diesem Fall wird der worst case von Quicksort dargestellt');
             schritt++;
             break;
         case 3:
-            einblenden(pivot,9);
+            showKlammer(1,9);
+            setText('Wenn Quicksort eine sortierte Liste bekommt, muss in jedem Ablauf die gesamte Liste betrachtet werden, was zu einer schlechten Effizienz führt');
             schritt++;
             break;
         case 4:
+            setText('Es wird eine Zahl als Referenz zum sortieren ausgesucht, diese Zahl wird "Pivot" genannt');
+            schritt++;
+            break;
+        case 5:
+            setText('Das Pivot wird normalerweise zufällig ausgewählt. In diesem Fall wird immer die Zahl rechts außen gewählt.');
+            einblenden(pivot,9);
+            schritt++;
+            break;
+        case 6:
+            setText('Als nächstes wird ein Marker an die Stelle ganz links und ganz rechts gesetzt.');
             einblenden(zeig1,1);
             einblenden(zeig2,8);
             schritt++;
             break;
-        case 5:
-            animateLeft(zeig1,2);
-            schritt++;
-            break;
-        case 6:
-            animateLeft(zeig1,3);
-            schritt++;
-            break;
         case 7:
-            animateLeft(zeig1,4);
+            setText('Der Quicksort Algorithmus verwendet diese Markierungen, um wiederholt Runden von Operationen rekursiv durchzuführen.');
             schritt++;
             break;
         case 8:
-            animateLeft(zeig1,5);
+            setText('Die linke Markierung bewegt sich nach rechts.');
+            animateLeft(zeig1,2);
             schritt++;
             break;
         case 9:
-            animateLeft(zeig1,6);
+            animateLeft(zeig1,3);
             schritt++;
             break;
         case 10:
-            animateLeft(zeig1,7);
+            animateLeft(zeig1,4);
             schritt++;
             break;
         case 11:
+            animateLeft(zeig1,5);
+            schritt++;
+            break;
+        case 12:
+            animateLeft(zeig1,6);
+            schritt++;
+            break;
+        case 13:
+            animateLeft(zeig1,7);
+            schritt++;
+            break;
+        case 14:
+            setText('Selbst wenn die linke Markierung in die rechte Markierung fährt, hört sie nicht auf. Sie unterscheidet sich von der rechten Markierung in dieser Hinsicht.');
             setDivHalf(zeig1);
             setDivHalf(zeig2);
             animateLeft(zeig1,8);
             animateLeft(zeig2,8.5);
             schritt++;
             break;
-        case 12:
+        case 15:
+            setText('Wenn die linke Markierung eine Zahl erreicht, die größer oder gleich der Pivotnummer ist, hört sie auf sich zu bewegen.');
             setDivHalf(pivot);
             animateLeft(zeig1,9);
             animateLeft(pivot,9.5);
@@ -224,68 +242,80 @@ function buttonItertator() {
             animateLeft(zeig2,8);
             schritt++;
             break;
-        case 13:
+        case 16:
+            setText('Wenn die linke Markierung den rechten Rand der Zielsequenz erreicht, stoppt sie.');
+            schritt++;
+            break;
+        case 17:
+            setText('Das bedeutet, dass die Pivotnummer die größte Zahl in der Zielsequenz ist.');
             setGray(zeig1);
             schritt++;
             break;
-        case 14:
+        case 18:
+            setText('Als nächstes würde die rechte Markierung bewegt werden, aber wenn sie von der linken Markierung passiert wurde, endet sie ohne sich zu bewegen.');
             setGray(zeig2);
             schritt++;
             break;
-        case 15:
+        case 19:
+            setText('Wenn die linke Markierung den rechten Rand der Zielsequenz erreicht hat, wird die Pivotnummer als vollständig sortiert angesehen und die Runde der Operationen endet.');
             ausblenden(zeig1);
             ausblenden(zeig2);
             ausblenden(pivot);
             setGray(div8);
             schritt++;
             break;
-        case 16:
+        case 20:
+            setText('Danach werden die Vorgänge wiederholt, bis alle Zahlen vollständig sortiert sind.');
             klammerAusblenden();
             schritt++;
             break;
-        case 17:
+        case 21:
+            setText('In unserem Fall sind die Zahlen schon sortiert und der Vorgang wird so oft wiederholt bis alle Zahlen abgearbeitet sind.');
+            schritt++;
+            break;
+        case 22:
             showKlammer(1,8);
             schritt++;
             break;
-        case 18:
+        case 23:
             einblenden(pivot,8);
             schritt++;
             break;
-        case 19:
+        case 24:
             setZeig2Normal();
             setZeig1Normal();
             einblenden(zeig1,1);
             einblenden(zeig2,7);
             schritt++;
             break;
-        case 20:
+        case 25:
             animateLeft(zeig1,2);
             schritt++;
             break;
-        case 21:
+        case 26:
             animateLeft(zeig1,3);
             schritt++;
             break;
-        case 22:
+        case 27:
             animateLeft(zeig1,4);
             schritt++;
             break;
-        case 23:
+        case 28:
             animateLeft(zeig1,5);
             schritt++;
             break;
-        case 24:
+        case 29:
             animateLeft(zeig1,6);
             schritt++;
             break;
-        case 25:
+        case 30:
             setDivHalf(zeig1);
             animateLeft(zeig1,7);
             setDivHalf(zeig2);
             animateLeft(zeig2,7.5);
             schritt++;
             break;
-        case 26:
+        case 31:
             setDivHalf(pivot);
             animateLeft(pivot,8.5);
             animateLeft(zeig1,8);
@@ -293,64 +323,64 @@ function buttonItertator() {
             animateLeft(zeig2,7);
             schritt++;
             break;
-        case 27:
+        case 32:
             setGray(zeig1);
             schritt++;
             break;
-        case 28:
+        case 33:
             setGray(zeig2);
             schritt++;
             break;
-        case 29:
+        case 34:
             ausblenden(zeig1);
             ausblenden(zeig2);
             ausblenden(pivot);
             setGray(div6);
             schritt++;
             break;
-        case 30:
+        case 35:
             klammerAusblenden();
             schritt++;
             break;
-        case 31:
+        case 36:
             showKlammer(1,7);
             schritt++;
             break;
-        case 32:
+        case 37:
             einblenden(pivot,7);
             schritt++;
             break;
-        case 33:
+        case 38:
             setZeig2Normal();
             setZeig1Normal();
             einblenden(zeig1,1);
             einblenden(zeig2,6);
             schritt++;
             break;
-        case 34:
+        case 39:
             animateLeft(zeig1,2);
             schritt++;
             break;
-        case 35:
+        case 40:
             animateLeft(zeig1,3);
             schritt++;
             break;
-        case 36:
+        case 41:
             animateLeft(zeig1,4);
             schritt++;
             break;
-        case 37:
+        case 42:
             animateLeft(zeig1,5);
             schritt++;
             break;
-        case 38:
+        case 43:
             setDivHalf(zeig1);
             setDivHalf(zeig2);
             animateLeft(zeig1,6);
             animateLeft(zeig2,6.5);
             schritt++;
             break;
-        case 39:
+        case 44:
             setDivHalf(pivot);
             animateLeft(zeig1,7);
             animateLeft(pivot,7.5);
@@ -358,60 +388,60 @@ function buttonItertator() {
             animateLeft(zeig2,6);
             schritt++;
             break;
-        case 40:
+        case 45:
             setGray(zeig1);
             schritt++;
             break;
-        case 41:
+        case 46:
             setGray(zeig2);
             schritt++;
             break;
-        case 42:
+        case 47:
             ausblenden(zeig1);
             ausblenden(zeig2);
             ausblenden(pivot);
             setGray(div4);
             schritt++;
             break;
-        case 43:
+        case 48:
             klammerAusblenden();
             schritt++;
             break;
-        case 44:
+        case 49:
             showKlammer(1,6);
             schritt++;
             break;
-        case 45:
+        case 50:
             einblenden(pivot,6);
             schritt++;
             break;
-        case 46:
+        case 51:
             setZeig2Normal();
             setZeig1Normal();
             einblenden(zeig1,1);
             einblenden(zeig2,5);
             schritt++;
             break;
-        case 47:
+        case 52:
             animateLeft(zeig1,2);
             schritt++;
             break;
-        case 48:
+        case 53:
             animateLeft(zeig1,3);
             schritt++;
             break;
-        case 49:
+        case 54:
             animateLeft(zeig1,4);
             schritt++;
             break;
-        case 50:
+        case 55:
             setDivHalf(zeig1);
             setDivHalf(zeig2);
             animateLeft(zeig1,5);
             animateLeft(zeig2,5.5);
             schritt++;
             break;
-        case 51:
+        case 56:
             setDivHalf(pivot);
             animateLeft(zeig1,6);
             animateLeft(pivot,6.5);
@@ -419,56 +449,56 @@ function buttonItertator() {
             animateLeft(zeig2,5);
             schritt++;
             break;
-        case 52:
+        case 57:
             setGray(zeig1);
             schritt++;
             break;
-        case 53:
+        case 58:
             setGray(zeig2);
             schritt++;
             break;
-        case 54:
+        case 59:
             ausblenden(zeig1);
             ausblenden(zeig2);
             ausblenden(pivot);
             setGray(div7);
             schritt++;
             break;
-        case 55:
+        case 60:
             klammerAusblenden();
             schritt++;
             break;
-        case 56:
+        case 61:
             showKlammer(1,5);
             schritt++;
             break;
-        case 57:
+        case 62:
             einblenden(pivot,5);
             schritt++;
             break;
-        case 58:
+        case 63:
             setZeig2Normal();
             setZeig1Normal();
             einblenden(zeig1,1);
             einblenden(zeig2,4);
             schritt++;
             break;
-        case 59:
+        case 64:
             animateLeft(zeig1,2);
             schritt++;
             break;
-        case 60:
+        case 65:
             animateLeft(zeig1,3);
             schritt++;
             break;
-        case 61:
+        case 66:
             setDivHalf(zeig1);
             setDivHalf(zeig2);
             animateLeft(zeig1,4);
             animateLeft(zeig2,4.5);
             schritt++;
             break;
-        case 62:
+        case 67:
             setDivHalf(pivot);
             animateLeft(zeig1,5);
             animateLeft(pivot,5.5);
@@ -476,52 +506,52 @@ function buttonItertator() {
             animateLeft(zeig2,4);
             schritt++;
             break;
-        case 63:
+        case 68:
             setGray(zeig1);
             schritt++;
             break;
-        case 64:
+        case 69:
             setGray(zeig2);
             schritt++;
             break;
-        case 65:
+        case 70:
             ausblenden(zeig1);
             ausblenden(zeig2);
             ausblenden(pivot);
             setGray(div1);
             schritt++;
             break;
-        case 66:
+        case 71:
             klammerAusblenden();
             schritt++;
             break;
-        case 67:
+        case 72:
             showKlammer(1,4);
             schritt++;
             break;
-        case 68:
+        case 73:
             einblenden(pivot,4);
             schritt++;
             break;
-        case 69:
+        case 74:
             setZeig2Normal();
             setZeig1Normal();
             einblenden(zeig1,1);
             einblenden(zeig2,3);
             schritt++;
             break;
-        case 70:
+        case 75:
             animateLeft(zeig1,2);
             schritt++;
             break;
-        case 71:
+        case 76:
             setDivHalf(zeig1);
             setDivHalf(zeig2);
             animateLeft(zeig1,3);
             animateLeft(zeig2,3.5);
             schritt++;
             break;
-        case 72:
+        case 77:
             setDivHalf(pivot);
             animateLeft(zeig1,4);
             animateLeft(pivot,4.5);
@@ -529,48 +559,48 @@ function buttonItertator() {
             animateLeft(zeig2,3);
             schritt++;
             break;
-        case 73:
+        case 78:
             setGray(zeig1);
             schritt++;
             break;
-        case 74:
+        case 79:
             setGray(zeig2);
             schritt++;
             break;
-        case 75:
+        case 80:
             ausblenden(zeig1);
             ausblenden(zeig2);
             ausblenden(pivot);
             setGray(div3);
             schritt++;
             break;
-        case 76:
+        case 81:
             klammerAusblenden();
             schritt++;
             break;
-        case 77:
+        case 82:
             showKlammer(1,3);
             schritt++;
             break;
-        case 78:
+        case 83:
             einblenden(pivot,3);
             schritt++;
             break;
-        case 79:
+        case 84:
             setZeig2Normal();
             setZeig1Normal();
             einblenden(zeig1,1);
             einblenden(zeig2,2);
             schritt++;
             break;
-        case 80:
+        case 85:
             setDivHalf(zeig1);
             setDivHalf(zeig2);
             animateLeft(zeig1,2);
             animateLeft(zeig2,2.5);
             schritt++;
             break;
-        case 81:
+        case 86:
             setDivHalf(pivot);
             animateLeft(zeig1,3);
             animateLeft(pivot,3.5);
@@ -578,34 +608,34 @@ function buttonItertator() {
             animateLeft(zeig2,2);
             schritt++;
             break;
-        case 82:
+        case 87:
             setGray(zeig1);
             schritt++;
             break;
-        case 83:
+        case 88:
             setGray(zeig2);
             schritt++;
             break;
-        case 84:
+        case 89:
             ausblenden(zeig1);
             ausblenden(zeig2);
             ausblenden(pivot);
             setGray(div2);
             schritt++;
             break;
-        case 85:
+        case 90:
             klammerAusblenden();
             schritt++;
             break;
-        case 86:
+        case 91:
             showKlammer(1,2);
             schritt++;
             break;
-        case 87:
+        case 92:
             einblenden(pivot,2);
             schritt++;
             break;
-        case 88:
+        case 93:
             setZeig2Normal();
             setZeig1Normal();
             einblenden(zeig1,1);
@@ -614,7 +644,7 @@ function buttonItertator() {
             setDivHalf(zeig2);
             schritt++;
             break;
-        case 89:
+        case 94:
             setDivHalf(pivot);
             animateLeft(zeig1,2);
             animateLeft(pivot,2.5);
@@ -622,39 +652,41 @@ function buttonItertator() {
             animateLeft(zeig2,1);
             schritt++;
             break;
-        case 90:
+        case 95:
             setGray(zeig1);
             schritt++;
             break;
-        case 91:
+        case 96:
             setGray(zeig2);
             schritt++;
             break;
-        case 92:
+        case 97:
             ausblenden(zeig1);
             ausblenden(zeig2);
             ausblenden(pivot);
             setGray(div5);
             schritt++;
             break;
-        case 93:
+        case 98:
             klammerAusblenden();
             schritt++;
             break;
-        case 94:
+        case 99:
             showKlammer(1,1);
             schritt++;
             break;
-        case 95:
+        case 100:
+            setText('Wenn die Zielsequenz nur eine Zahl hat, wird sie als vollständig sortiert betrachtet.');
             setGray(div9);
             schritt++;
             break;
-        case 96:
+        case 101:
             klammerAusblenden();
+            setText('Alle Zahlen wurden vollständig abgearbeitet');
             schritt++;
             break;
-        case 97:
-            setText('Ende');
+        case 102:
+            setText('Damit ist die Darstellung des worst case abgeschlossen und man merkt das es sich um die schlechteste Effizienz handelt.');
             disableButton($('#buttonNext'));
             break;
     }
